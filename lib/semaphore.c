a2538955b9d2de4e1bb712bfee373357b06ee48d
Omar-Elhakim <omarelhakim@icloud.com>
2024-12-12 02:51:48 +0200

    semaphore needs more work ,removed unneeded TODO

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -4,10 +4,18 @@
 
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
-	//TODO: [PROJECT'24.MS3 - #02] [2] USER-LEVEL SEMAPHORE - create_semaphore
-	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("create_semaphore is not implemented yet");
-	//Your Code is Here...
+	
+	struct __semdata *semdata={0};
+	//todo save the semaphore in the env in some way
+	semdata->count=value;
+	strcpy(semdata->name,semaphoreName);
+	semdata->lock=0;
+	LIST_INIT(&(semdata->queue));
+	struct semaphore s={
+		semdata
+	};
+	int id=sys_createSharedObject(semaphoreName,sizeof(struct semaphore),1,&s);
+	return s;
 }
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
@@ -19,18 +27,28 @@ struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 
 void wait_semaphore(struct semaphore sem)
 {
-	//TODO: [PROJECT'24.MS3 - #04] [2] USER-LEVEL SEMAPHORE - wait_semaphore
-	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("wait_semaphore is not implemented yet");
-	//Your Code is Here...
+	int keyw=1;
+	while(xchg(&(sem.semdata->lock),keyw)!=0);
+	if(--(sem.semdata->count)<0)
+	{
+		// LIST_INSERT_TAIL(&(sem.semdata->queue),myEnv);
+		// enqueue(&(sem.semdata->queue),myEnv);
+		sem.semdata->lock=0;
+		//todo block the env
+	}
+	sem.semdata->lock=0;
 }
 
 void signal_semaphore(struct semaphore sem)
 {
-	//TODO: [PROJECT'24.MS3 - #05] [2] USER-LEVEL SEMAPHORE - signal_semaphore
-	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("signal_semaphore is not implemented yet");
-	//Your Code is Here...
+	int keys=1;
+	while(xchg(&(sem.semdata->lock),keys)!=0);
+	if(++(sem.semdata->count)<=0)
+	{
+		// struct Env *env= dequeue(&(sem.semdata->queue));		
+		// sched_insert_ready(env);//not sure
+	}
+	sem.semdata->lock=0;
 }
 
 int semaphore_count(struct semaphore sem)
