2e8e95400b6c530f88abb4d77b32e129726681b1
Omar-Elhakim <omarelhakim@icloud.com>
2024-12-13 03:55:17 +0200

    still not done

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -9,47 +9,56 @@
 //todo
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
-	
-	struct __semdata *semdata={0};
+	cprintf("Create Semaphore\n");
+	struct __semdata *semdata=smalloc(semaphoreName,sizeof(struct semaphore),1);
 	semdata->count=value;
+
 	strcpy(semdata->name,semaphoreName);
 	semdata->lock=0;
-	// sys_init_queue((semdata->queue))
+	sys_init_queue(&(semdata->queue));
 	struct semaphore s={
 		semdata
 	};
-	smalloc(semaphoreName,sizeof(struct semaphore),1);
+
 	return s;
 }
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
+	cprintf("Get Semaphore\n");
 	return *((struct semaphore *)(sget(ownerEnvID,semaphoreName))); 
 }
 
 void wait_semaphore(struct semaphore sem)
 {
-	int keyw=1;
-	while(xchg(&(sem.semdata->lock),keyw)!=0);
+	cprintf("Wait sem name: %s\n",sem.semdata->name);
+	while(xchg(&(sem.semdata->lock),1)!=0);
 	if(--(sem.semdata->count)<0)
 	{
-		// sys_enqueue(&(sem.semdata->queue),myEnv);
+		cprintf("inside if\n");
+		struct Env *env= sys_get_cpu_proc();
+		cprintf("inside if\n");
+		sys_enqueue(&(sem.semdata->queue),env);
+		cprintf("inside if\n");
+		env->env_status=ENV_BLOCKED;
+		cprintf("inside if\n");
 		sem.semdata->lock=0;
-		// struct Env env= sys_get_cpu_proccess();
-		// env->env_status=ENV_BLOCKED;
+		cprintf("end of if if\n");
 	}
-	sem.semdata->lock=0;
+	else sem.semdata->lock=0;
+	cprintf("end of wait\n");
 }
 
 void signal_semaphore(struct semaphore sem)
 {
+	cprintf("signal\n");
 	int keys=1;
-	while(xchg(&(sem.semdata->lock),keys)!=0);
+	while(xchg(&(sem.semdata->lock),1)!=0);
 	if(++(sem.semdata->count)<=0)
 	{
-		// struct Env *env= dequeue(&(sem.semdata->queue));		
-		// sched_insert_ready(env);//not sure
+		struct Env *env= sys_dequeue(&(sem.semdata->queue));		
+		sys_sched_insert_ready(env);
 	}
-	sem.semdata->lock=0;
+	else sem.semdata->lock=0;
 }
 
 int semaphore_count(struct semaphore sem)
