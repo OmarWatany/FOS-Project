1b1ac4b249cdf34dfde9430a1fb2749215c352dc
Omar-Elhakim <omarelhakim@icloud.com>
2024-12-14 19:07:41 +0200

    is better , the logic is right but there is a memory protection issue

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -9,48 +9,47 @@
 //todo
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
-	cprintf("Create Semaphore\n");
+	// cprintf("Create Semaphore\n");
 	struct __semdata *semdata=smalloc(semaphoreName,sizeof(struct semaphore),1);
 	semdata->count=value;
-
 	strcpy(semdata->name,semaphoreName);
 	semdata->lock=0;
 	sys_init_queue(&(semdata->queue));
-	struct semaphore s={
-		semdata
-	};
 
-	return s;
+	struct semaphore *s=smalloc(semaphoreName,sizeof(struct semaphore),1);
+	s->semdata=semdata;
+	return *s;
 }
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
-	cprintf("Get Semaphore\n");
-	return *((struct semaphore *)(sget(ownerEnvID,semaphoreName))); 
+	// cprintf("Get Semaphore %s\n",semaphoreName);
+	// struct __semdata *semdata=((struct __semdata *)(sget(ownerEnvID,semaphoreName)));
+	// struct semaphore s;
+	// s.semdata=semdata;
+	// return s; 
+	struct semaphore *s=((struct semaphore *)(sget(ownerEnvID,semaphoreName)));
+	return *s;
+
 }
 
 void wait_semaphore(struct semaphore sem)
 {
-	cprintf("Wait sem name: %s\n",sem.semdata->name);
+	// cprintf("Wait sem name: %s\n",sem.semdata->name);
 	while(xchg(&(sem.semdata->lock),1)!=0);
 	if(--(sem.semdata->count)<0)
 	{
-		cprintf("inside if\n");
 		struct Env *env= sys_get_cpu_proc();
-		cprintf("inside if\n");
 		sys_enqueue(&(sem.semdata->queue),env);
-		cprintf("inside if\n");
-		env->env_status=ENV_BLOCKED;
-		cprintf("inside if\n");
 		sem.semdata->lock=0;
-		cprintf("end of if if\n");
+		env->env_status=ENV_BLOCKED;
 	}
 	else sem.semdata->lock=0;
-	cprintf("end of wait\n");
+	// cprintf("end of wait\n");
 }
 
 void signal_semaphore(struct semaphore sem)
 {
-	cprintf("signal\n");
+	// cprintf("signal\n");
 	int keys=1;
 	while(xchg(&(sem.semdata->lock),1)!=0);
 	if(++(sem.semdata->count)<=0)
