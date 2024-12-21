1992f784b4fd735675a01670b7eb1ff1b29f8c2b
OmarWatany <watany6500@gmail.com>
2024-12-21 22:24:15 +0200

    change semaphore allocation method

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -3,21 +3,39 @@
 #include "inc/lib.h"
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
-	struct semaphore *s = smalloc(semaphoreName,sizeof(struct semaphore)+sizeof(struct __semdata),1);
-	s->semdata = (void *)s+sizeof(struct semaphore);
-	struct __semdata *semdata = s->semdata;
-
-	semdata->count=value;
-	strcpy(semdata->name,semaphoreName);
-	semdata->lock=0;
+#if 1
+	struct __semdata *semdata = smalloc(semaphoreName,sizeof(struct __semdata),1) ;
 	sys_init_queue(&(semdata->queue));
-	return *s;
+	strcpy(semdata->name,semaphoreName);
+	semdata->count=value;
+	semdata->lock=0;
+	return (struct semaphore) {
+		.semdata = semdata,
+	};
+	
+#else
+	/* struct semaphore *s = smalloc(semaphoreName,sizeof(struct semaphore)+sizeof(struct __semdata),1); */
+	/* s->semdata = (void *)s+sizeof(struct semaphore); */
+	/* struct __semdata *semdata = s->semdata; */
+
+	/* semdata->count=value; */
+	/* strcpy(semdata->name,semaphoreName); */
+	/* semdata->lock=0; */
+	/* sys_init_queue(&(semdata->queue)); */
+	/* return *s; */
+#endif
 }
 
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
+#if 1
+	return (struct semaphore){
+		.semdata = sget(ownerEnvID,semaphoreName),
+	};
+#else
 	struct semaphore *s=((struct semaphore *)(sget(ownerEnvID,semaphoreName)));
 	return *s;
+#endif
 }
 
 void wait_semaphore(struct semaphore sem)
